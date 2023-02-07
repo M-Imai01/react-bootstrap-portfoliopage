87fc0a1165526c6a006c090ead2cd9611613e662
M-Imai01 <fruitjuice877@gmail.com>
2023-02-07 11:11:14 +0900

    Header・WorksPageの作成

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './components/Header';
+import HomePage from './components/HomePage';
+import WorksPage from './components/WorksPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <HomePage />
+      <WorksPage />
     </div>
   );
 }

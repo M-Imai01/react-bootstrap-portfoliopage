0d61182b05e8e172df50531069dd7aa88e3bf08f
M-Imai01 <fruitjuice877@gmail.com>
2023-02-07 10:50:36 +0900

    header部分の作成

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <h3>ポートフォリオ</h3>
+      </div>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Works</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+
+      </nav>
+    </header>
+  )
+}
+
+export default Header

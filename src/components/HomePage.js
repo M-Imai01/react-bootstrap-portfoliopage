fed6a961940f23c5460f0198355accc18a448858
M-Imai01 <fruitjuice877@gmail.com>
2023-02-07 19:10:01 +0900

    デザインスキルアイテムの編集

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,12 +6,18 @@ import profileImage from "../Images/sibainu.jpg";
 import DesignImage from "../Images/Design.svg";
 import CodingImage from "../Images/Coding.svg";
 import OtherImage from "../Images/Other.svg";
+import photoshopImage from "../Images/photoshop.svg";
+import illustratorImage from "../Images/illustrator.svg";
+import xdImage from "../Images/xd.svg";
+import PremiereProImage from "../Images/PremierePro.svg";
+
 
 
 const HomePage = () => {
   return (
 
     <div className="container text-center">
+      <p>Imai Mitsuaki</p>
       <h1>今井　光明</h1>
 
       <img src={profileImage} className="profileImage" />
@@ -26,45 +32,56 @@ const HomePage = () => {
       <h1 class="title">スキル</h1>
       <div class="d-flex flex-row">
 
-        <div className="skillbox">
+        <div className="skillbox col-4">
           <h3>-Design-</h3>
             <img src={DesignImage} className="skill-thema" />
           <div class="d-flex flex-column skill-item">
             <div class="services d-flex flex-row">
-                <img src={reactImage} className="skill-img" />
+                <img src={photoshopImage} className="skill-img" />
               <span class="d-flex flex-column">
-                <h5>React</h5>
-                <p>Reactがつかえます</p>
+                <h5>Photoshop</h5>
+                <p>写真のトリミング、色彩調整など基本操作可能</p>
               </span>
             </div>
           </div>
           <div class="d-flex flex-column skill-item">
           <div class="services d-flex flex-row">
-            <img src={jsImage} className="skill-img" />
+                <img src={illustratorImage} className="skill-img" />
             <span class="d-flex flex-column">
-              <h5>HTML/CSS</h5>
-              <p>HTML/CSSがつかえます</p>
+                  <h5>illustrator</h5>
+              <p>ペンツール・ベジェ曲線の作成や、基本操作可能</p>
             </span>
           </div>
           </div>
           <div class="d-flex flex-column skill-item">
           <div class="services d-flex flex-row">
-            <img src={firebaseImage} className="skill-img" />
+                <img src={xdImage} className="skill-img" />
             <span class="d-flex flex-column">
-              <h5>Firebase</h5>
-              <p>Firebaseがつかえます</p>
+              <h5>XD</h5>
+              <p>サイトのトップページデザインカンプ作成可能</p>
             </span>
           </div>
           </div>
+            <div class="d-flex flex-column skill-item">
+              <div class="services d-flex flex-row">
+                <img src={PremiereProImage} className="skill-img" />
+                <span class="d-flex flex-column">
+                  <h5>Premiere Pro</h5>
+                  <p>動画の切り貼り、テロップの挿入など5~7分ほどの動画編集可能</p>
+                </span>
+              </div>
+            </div>
         </div>
 
-      <div className="skillbox">
+      <div className="skillbox col-4">
         <h3>-Coding-</h3>
+        <span className="skill_thema-box">
             <img src={CodingImage} className="skill-thema" />
+        </span>
         <div class="d-flex flex-column skill-item">
-          <div class="services d-flex flex-row">
+              <div class="services d-flex flex-row">
             <img src={reactImage} className="skill-img" />
-            <span class="d-flex flex-column">
+                <span class="d-flex flex-column">
               <h5>React</h5>
               <p>Reactがつかえます</p>
             </span>
@@ -90,9 +107,11 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="skillbox">
+      <div className="skillbox col-4">
         <h3>-Other-</h3>
+          <span className="skill_thema-box">
             <img src={OtherImage} className="skill-thema" />
+          </span>
         <div class="d-flex flex-column skill-item">
           <div class="services d-flex flex-row">
             <img src={reactImage} className="skill-img" />

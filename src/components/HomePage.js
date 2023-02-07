77548e60e5e5dcec65289b0bf5a557ef9e17a88b
M-Imai01 <fruitjuice877@gmail.com>
2023-02-07 14:16:21 +0900

    スキルボックスの調整とフォントの実装

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,12 +18,11 @@ const HomePage = () => {
       <p>職歴:家電量販店の販売員、手芸キットメーカーのWeb更新担当、インターネットサービス企業を経験。</p>
       <p>2021年6月よりWebデザインの職業訓練校にてデザインやフロントエンドコーディングを学び、Web業界へ挑戦中。</p>
 
-      <section id="skill">
-        <h1 class="title">スキル</h1>
-        <div class="d-flex flex-row">
+    <section id="skill">
+      <h1 class="title">スキル</h1>
+      <div class="d-flex flex-row">
 
         <div className="skillbox"><h3>-Design-</h3>
-
           <div class="d-flex flex-column skill-item">
             <div class="services d-flex flex-row">
               <img src={reactImage} className="img-style" />
@@ -33,7 +32,6 @@ const HomePage = () => {
               </span>
             </div>
           </div>
-
           <div class="d-flex flex-column skill-item">
           <div class="services d-flex flex-row">
             <img src={jsImage} className="img-style" />
@@ -43,7 +41,6 @@ const HomePage = () => {
             </span>
           </div>
           </div>
-
           <div class="d-flex flex-column skill-item">
           <div class="services d-flex flex-row">
             <img src={firebaseImage} className="img-style" />
@@ -55,8 +52,68 @@ const HomePage = () => {
           </div>
         </div>
 
+      <div className="skillbox"><h3>-Coding-</h3>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={reactImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>React</h5>
+              <p>Reactがつかえます</p>
+            </span>
+          </div>
         </div>
-      </section>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={jsImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>HTML/CSS</h5>
+              <p>HTML/CSSがつかえます</p>
+            </span>
+          </div>
+        </div>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={firebaseImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>Firebase</h5>
+              <p>Firebaseがつかえます</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div className="skillbox"><h3>-Other-</h3>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={reactImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>React</h5>
+              <p>Reactがつかえます</p>
+            </span>
+          </div>
+        </div>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={jsImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>HTML/CSS</h5>
+              <p>HTML/CSSがつかえます</p>
+            </span>
+          </div>
+        </div>
+        <div class="d-flex flex-column skill-item">
+          <div class="services d-flex flex-row">
+            <img src={firebaseImage} className="img-style" />
+            <span class="d-flex flex-column">
+              <h5>Firebase</h5>
+              <p>Firebaseがつかえます</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </section>
 
     </div>
 
